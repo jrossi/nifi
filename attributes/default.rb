c158988fa19b7a26591d75304292c4d879ad145a
Mercedes Coyle <mcoyle@ulive.com>
2015-10-05 17:30:07 -0700

    Removed enable test and jdk flavor

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,3 @@
 default['nifi']['package_url'] = 'http://apache.mirrors.tds.net/nifi/0.3.0/nifi-0.3.0-bin.tar.gz'
 default['nifi']['version']     = '0.3.0'
-default['java']['install_flavor'] = 'openjdk'
 default['java']['jdk_version'] = '7'
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,18 +1,6 @@
 require 'spec_helper'
 
-describe 'nifi::server' do
-
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
-
-end
-
 describe service('nifi') do
-  it {should be_enabled}
   it {should be_running}
 end
 
