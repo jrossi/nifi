7b8164b2c603aaa4122586cda6360bdb711e6022
Mercedes Coyle <mcoyle@ulive.com>
2015-12-18 14:15:27 -0800

    Added new url for latest nifi version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,5 @@
-default['nifi']['package_url'] = 'http://apache.mirrors.tds.net/nifi/0.3.0/nifi-0.3.0-bin.tar.gz'
+# default['nifi']['package_url'] = 'http://apache.mirrors.pair.com/nifi/0.4.0/nifi-0.4.0-bin.tar.gz'
+default['nifi']['package_url'] = 'https://archive.apache.org/dist/nifi/0.3.0/nifi-0.3.0-bin.tar.gz'
+# default['nifi']['version']     = '0.4.0'
 default['nifi']['version']     = '0.3.0'
 default['java']['jdk_version'] = '7'
